71da7d0852650688eb4e80f9b32da8ee4c8ac732
Endrew Barbosa <endrew.ab@hotmail.com>
2023-04-18 11:59:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import tarefas
 
 if __name__ == '__main__':
     # get_tarefas deve ser executado primeiro para adquirir os ids
-    # tarefas.get_tarefas()
+    tarefas.get_tarefas()
     listas.listar()
