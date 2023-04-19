5c3c6370003018619bd8da727e6ef70165665d0e
Endrew Barbosa <endrew.ab@hotmail.com>
2023-04-19 11:20:57 -0300

    Update join_function.py

--- join_function.py
+++ join_function.py
@@ -1,5 +1,10 @@
 def join_data(lista):
     lista_join = []
-    lista_join = '; '.join(lista)
+    empty_data = ''
+    for i, teste in enumerate(lista):
+        if teste != None:
+            lista_join = '; '.join(lista)
 
-    return lista_join
+            return lista_join
+
+    return empty_data
