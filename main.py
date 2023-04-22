68387e17453724ec07ba69b480f8946c9775eac2
Endrew Barbosa <endrew.ab@hotmail.com>
2023-04-22 18:14:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import beneficios_id
 
 if __name__ == '__main__':
     # get_tarefas e get_beneficios_id devem ser executados primeiros para adquirirem os ids
-    beneficios_id.get_beneficios_id()
     tarefas.get_tarefas()
+    beneficios_id.get_beneficios_id()
 
     # funções para buscar os dados por completo
     beneficios.get_beneficios()
