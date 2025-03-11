9478e0e70e2109481006b89a735fda00cb2015ea
ricardocmduarte <ricnewline@gmail.com>
2025-03-11 16:12:58 -0300

    Atualização do codigo em 11/03/2025.

--- dadosreader_env.py
+++ dadosreader_env.py
@@ -7,8 +7,8 @@ def read_env_file():
         'eAud_auxiliar', #database 
         'm1503249', #masp 
         'Ri11C@k06', #pswd 
-        'https://cecad365.webhook.office.com/webhookb2/2312fdce-cec5-4868-a4b4-69e73fbf51ba@e5d3ae7c-9b38-48de-a087-f6734a287574/IncomingWebhook/bd39c4b46106455face502ba6e38ffc6/1439ca6d-7a4e-4610-8706-675c6b9d3d6c', #teams 
-        'https://cecad365.webhook.office.com/webhookb2/6e239511-72d5-4b67-b246-72f816722fc1@e5d3ae7c-9b38-48de-a087-f6734a287574/IncomingWebhook/e5a935383c01424985495a92ce47f60d/1439ca6d-7a4e-4610-8706-675c6b9d3d6c' #teamsteste 
+        'https://cecad365.webhook.office.com/webhookb2/2312fdce-cec5-4868-a4b4-69e73fbf51ba@e5d3ae7c-9b38-48de-a087-f6734a287574/IncomingWebhook/7c9f244177d2426c839f846cf8652fca/1439ca6d-7a4e-4610-8706-675c6b9d3d6c/V2QfYPJi9pTUUxsT1nvG7qWlOqxXDwzF2XaoDRrsW_Tfk1', #teams 
+        'https://cecad365.webhook.office.com/webhookb2/6e239511-72d5-4b67-b246-72f816722fc1@e5d3ae7c-9b38-48de-a087-f6734a287574/IncomingWebhook/1ddb33365deb44529896324cf04fd150/1439ca6d-7a4e-4610-8706-675c6b9d3d6c/V2n1XGatpIAnTP9h73xr64m12iTFdosl-mQ3ru5s3b6-k1' #teamsteste 
     ]
     
     return listas_dados
